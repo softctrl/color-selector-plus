84eeea8660152c73c7c988f034923c93214752de
Manindra Moharana <mkmoharana29@gmail.com>
2011-08-29 01:34:07 +0530

    Testing how commit works. Minor changes to headers.

--- Color Selector PTool/src/colorSelector/ColorSelector.java
+++ Color Selector PTool/src/colorSelector/ColorSelector.java
@@ -1,21 +1,8 @@
 /*
  * Color Selector Plus
  * Advanced Color Selector tool for Processing IDE
- *
+ * 
  * Copyright (C) 2011 Manindra Moharana.
- *
- * Color Selector Plus is free software, you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * InitSketch is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package colorSelector;
@@ -63,8 +50,8 @@ public class ColorSelector extends JFrame implements KeyListener,
 	Editor editor;
 	String currentFile;
 
-	/**
-	 * Launch the application.
+	/*
+	 * Main method to launch it for testing
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
